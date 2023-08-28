50d42a2d2241ebc96c55381c53ef55539db387d3
Rakin Mohammad Sifullah <rakin.sifullah@gmail.com>
2023-08-28 07:59:19 +0600

    public, resources and routes

---
+++ CRUD/resources/views/show_data.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>CRUD</title>
+  </head>
+  <body>
+        <div class="container">
+            <a href="{{url('/add-data')}}" class="btn btn-primary my-3">Add Data</a>
+            @if(Session::has('msg'))
+            <p class="alert alert-success">{{ Session::get('msg') }}</p>
+            @endif
+        <table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($showData as $key=>$data)
+    <tr>
+      <td>{{$key+1}}</td>
+      <td>{{$data->name}}</td>
+      <td>{{$data->email}}</td>
+      <td>
+        <a href="{{url('/edit-data/'.$data->id)}}" class="btn btn-success">Edit</a>
+        <a href="{{url('/delete-data/'.$data->id)}}" class="btn btn-danger">Delete</a>
+      </td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+{{$showData->links()}}
+        </div>  
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
